1a3f8cdc7648ce7ac22e1d047865d0441c9a3f11
dorancostin <doran_costin@yahoo.com>
2012-10-29 09:39:23 +0200

    added junit to the dependencies

--- src/main/java/springLearning/App.java
+++ src/main/java/springLearning/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World -a!" );
+        System.out.println( "Hello World -c!" );
         System.out.println( "Hello World -b!" );
         System.out.println("inca o linie");
         System.out.println("linia 3");
