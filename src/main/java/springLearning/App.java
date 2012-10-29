cf6de9e963f507328684829910338e1e5dd160bd
dorancostin <doran_costin@yahoo.com>
2012-10-29 12:12:38 +0200

    asdas

--- src/main/java/springLearning/App.java
+++ src/main/java/springLearning/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World -c!" );
         System.out.println( "Hello World -d!" );
-        System.out.println("inca o linie abcd");
+        System.out.println("inca o linie abcd asfdas");
         System.out.println("linia 3");
         System.out.println( "Hello 123" );
         System.out.println( "Hello 233" );
